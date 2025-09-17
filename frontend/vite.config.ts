d52c0eea77ee8c1b850b2124fed0f0c345361493
Daniel Boeve <djboeve@gmail.com>
2025-09-17 06:20:04 -0700

    No logical change, forcing a reformat before logical changes in following commit.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,27 +1,29 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    strictPort: true,
-    proxy: {
-      // Proxy API to backend dev server when running locally
-      "/api": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
+export default defineConfig(() => {
+  return {
+    plugins: [react()],
+    server: {
+      port: 5173,
+      strictPort: true,
+      proxy: {
+        // Proxy API to backend dev server when running locally
+        "/api": {
+          target: "http://localhost:8000",
+          changeOrigin: true,
+        },
       },
     },
-  },
-  preview: {
-    port: 5173,
-    strictPort: true,
-  },
-  test: {
-    environment: "jsdom",
-    globals: true,
-    setupFiles: ["./src/setupTests.ts"],
-    include: ["src/**/*.{test,spec}.tsx"],
-  },
+    preview: {
+      port: 5173,
+      strictPort: true,
+    },
+    test: {
+      environment: "jsdom",
+      globals: true,
+      setupFiles: ["./src/setupTests.ts"],
+      include: ["src/**/*.{test,spec}.tsx"],
+    },
+  };
 });
